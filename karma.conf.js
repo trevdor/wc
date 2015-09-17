049437984c415a517081a557f2a6e16c258708b2
Trevor Farlow <tfarlow@rallydev.com>
2015-09-17 10:39:42 -0600

    chore(CI): First go at Travis config

--- karma.conf.js
+++ karma.conf.js
@@ -2,7 +2,7 @@ var path = require("path");
 
 module.exports = function(config) {
   config.set({
-    browsers: ['Chrome'],
+    [ process.env.CONTINUOUS_INTEGRATION ? 'Firefox' : 'Chrome' ],
     files: [
       'test/**/*.spec.js'
     ],
