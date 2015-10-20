e581af95dca82d92399103e89c436fdd88e97db6
Trevor Farlow <tfarlow@rallydev.com>
2015-10-20 11:45:18 -0600

    chore(test setup): Configure test setup to look more like the Redux testing example

--- test/utils/dom.js
+++ test/utils/dom.js
@@ -1,19 +1,14 @@
 import jsdom from 'jsdom';
 
-// setup the simplest document possible
-const doc = jsdom.jsdom('<!doctype html><html><body></body></html>');
-
-// get the window object out of the document
-const win = doc.defaultView;
-
 // set globals for mocha that make access to document and window feel
 // natural in the test environment
-global.document = doc;
-global.window = win;
+global.document = jsdom('<!doctype html><html><body></body></html>');
+global.window = document.defaultView;
+global.navigator = global.window.navigator;
 
 // take all properties of the window object and also attach it to the
 // mocha global object
-propagateToGlobal(win);
+propagateToGlobal(window);
 
 // from mocha-jsdom https://github.com/rstacruz/mocha-jsdom/blob/master/index.js#L80
 function propagateToGlobal(window) {
